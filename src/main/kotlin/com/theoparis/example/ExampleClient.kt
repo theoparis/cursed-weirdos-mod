d2e22380727690107f559044ee73f380f3e809bd
Theo Paris <theoparisdesigns@gmail.com>
2021-04-11 18:30:38 -0700

    Fixed example client error

--- src/main/kotlin/com/theoparis/example/ExampleClient.kt
+++ src/main/kotlin/com/theoparis/example/ExampleClient.kt
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.LogManager
 class ExampleClient : ClientModInitializer {
     companion object {
         @JvmStatic
-        val logger = LogManager.getFormatterLogger(InfiniteInsanityMod.MOD_ID)
+        val logger = LogManager.getFormatterLogger(ExampleMod.MOD_ID)
 
         // Client-side registration
 
